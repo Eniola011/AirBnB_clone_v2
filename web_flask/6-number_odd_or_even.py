7883e067c0cb2b856bc63dec71e4ab1ba6854c2a
Eniola011 <yettyagbalu@gmail.com>
2023-12-28 15:37:28 -0800

    resolved: 6-number_odd_or_even.py:46:80: E501 line too long (85 > 79 characters)

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,8 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     even_or_odd = 'even' if n % 2 == 0 else 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, even_or_odd=even_or_odd)
+    return render_template('6-number_odd_or_even.html',
+                           n=n, even_or_odd=even_or_odd)
 
 
 if __name__ == "__main__":
